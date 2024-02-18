0a0f7776cd0c18d76a285d33602a14a02d38b173
Subodh-27 <sbhawt@gmail.com>
2024-02-19 00:09:01 +0530

    Read User Input

---
+++ 03_readInput.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Read User Input"
+echo ""
+
+
+# echo "Enter your Name: "
+# read name
+# echo "The entered Name is:" $name
+
+# echo ""
+
+# read -p "Enter your Age: " age
+# echo "Your Age is:" $age
+
+# echo "Enter your Hobby: "
+# read
+# echo "Your Hobbys is:" $REPLY
+
+read -p "Username: " username
+read -sp "Password: " password
+echo "Username is:" $username "& Password is:" $password
